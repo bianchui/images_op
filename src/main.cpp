f575e6fa46232ac253d635e83c58d994f49b6590
bianchui <bianchui@gmail.com>
2022-07-29 21:15:20 +0800

    xcode_proj

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  images_op
+//
+//  Created by bianchui on 2022/7/29.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
