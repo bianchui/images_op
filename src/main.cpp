2328c74072060ca92ebf241258428323feda7f30
bianchui <bianchui@gmail.com>
2022-08-01 17:50:39 +0800

    write test png

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,74 @@
 //
 
 #include <iostream>
+#include "../lib/libpng-1.6.37/png.h"
+#include <unistd.h>
+
+bool writePng(const char* name, int w, int h, const void* data) {
+    FILE *fp = fopen(name, "wb");
+    if (!fp) {
+        return false;
+    }
+    png_structp png_ptr = nullptr;
+    png_infop info_ptr = nullptr;
+    bool success = false;
+    while (1) {
+        png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+
+        if (nullptr == png_ptr) {
+            break;
+        }
+
+        info_ptr = png_create_info_struct(png_ptr);
+        if (nullptr == info_ptr) {
+            break;
+        }
+        if (setjmp(png_jmpbuf(png_ptr))) {
+            break;
+        }
+        png_init_io(png_ptr, fp);
+        png_set_IHDR(png_ptr, info_ptr, w, h, 8, PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+        png_set_compression_level(png_ptr, 9);
+        png_write_info(png_ptr, info_ptr);
+        png_set_packing(png_ptr);
+
+        png_const_bytep ptr = (png_const_bytep)data;
+        for (int i = 0; i < h; ++i) {
+            png_write_row(png_ptr, ptr + i * w * 4);
+        }
+
+        png_write_end(png_ptr, info_ptr);
+
+        break;
+    }
+
+    if (png_ptr) {
+        png_destroy_write_struct(&png_ptr, &info_ptr);
+    }
+
+    fclose(fp);
+    
+    return success;
+}
+
+struct RGBA {
+    uint8_t r,g,b,a;
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    RGBA data[4*4];
+    
+    for (int i = 0; i < 16; ++i) {
+        data[i].r = 255;
+        data[i].g = 0;
+        data[i].b = 0;
+        data[i].a = i * 255 / 15;
+    }
+    
+    char cwd[1024];
+    printf("cwd: %s\n", getcwd(cwd, 1024));
+        
+    writePng("test.png", 4, 4, data);
+    
     return 0;
 }
