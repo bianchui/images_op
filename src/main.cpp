204af806c984ea83cc2f4cd9e8808089f3930d46
bianchui <bianchui@gmail.com>
2022-08-04 10:09:08 +0800

    gif: support disposalMode

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 #include <unistd.h>
 #include <memory>
 #include <string>
+#include <vector>
 
 bool writePng(const char* name, int w, int h, const void* data) {
     FILE *fp = fopen(name, "wb");
@@ -67,10 +68,31 @@ void printGIFError(const char* name, int err) {
     printf("gif: %s: error: %d(%s)", name, err, GifErrorString(err));
 }
 
+void saveSubImage(const char* name, int i, int width, int height, const void* image) {
+    std::string newName = name;
+    char buf[32];
+    sprintf(buf, "%d", i);
+    newName.append(buf);
+    newName.append(".png");
+    
+    writePng(newName.c_str(), width, height, image);
+}
+
+// https://docstore.mik.ua/orelly/web2/wdesign/ch23_05.htm
 void saveGIFFrames(GifFileType* GifFile, const char* name) {
-    std::unique_ptr<RGBA[]> image(new RGBA[GifFile->SWidth * GifFile->SHeight]);
+    std::vector<std::shared_ptr<RGBA[]>> images;
     const int width = GifFile->SWidth, height = GifFile->SHeight;
+    int doNotDispose = -1;
     for (int i = 0; i < GifFile->ImageCount; ++i) {
+        std::shared_ptr<RGBA[]> image;
+        bool imgPrepared = false;
+        if (i < images.size()) {
+            image = images[i];
+            saveSubImage(name, i + 100, width, height, image.get());
+            imgPrepared = true;
+        } else {
+            image = std::shared_ptr<RGBA[]>(new RGBA[width * height]);
+        }
         const auto& img = GifFile->SavedImages[i];
         
         /* Lets dump it - set the global variables required and do it: */
@@ -83,7 +105,9 @@ void saveGIFFrames(GifFileType* GifFile, const char* name) {
         /* check that the background color isn't garbage (SF bug #87) */
         auto bgColorIndex = GifFile->SBackGroundColor;
         if (bgColorIndex < 0 || bgColorIndex >= ColorMap->ColorCount) {
-            fprintf(stderr, "Background color out of range for colormap\n");
+            if (!imgPrepared) {
+                fprintf(stderr, "Background color out of range for colormap\n");
+            }
             bgColorIndex = 0;
         }
         
@@ -105,46 +129,96 @@ void saveGIFFrames(GifFileType* GifFile, const char* name) {
             .b = bgColor.Blue,
             .a = bgColorA,
         };
-
-        for (int y = 0; y < startY; ++y) {
-            auto line = image.get() + y * width;
-            for (int x = 0; x < width; x++) {
-                line[x] = bgRGBA;
+ 
+        if (!imgPrepared) {
+            for (int y = 0; y < startY; ++y) {
+                auto line = image.get() + y * width;
+                for (int x = 0; x < width; x++) {
+                    line[x] = bgRGBA;
+                }
             }
         }
         for (int y = 0, subheight = endY - startY; y < subheight; ++y) {
             auto line = image.get() + (y + startY) * width;
-            for (int x = 0; x < startX; ++x) {
-                line[x] = bgRGBA;
+            if (!imgPrepared) {
+                for (int x = 0; x < startX; ++x) {
+                    line[x] = bgRGBA;
+                }
             }
             auto subLine = line + startX;
             auto gifLine = img.RasterBits + y * img.ImageDesc.Width;
             for (int x = 0, subWidth = endX - startX; x < subWidth; ++x) {
+                const auto colorIndex = gifLine[x];
+                if (colorIndex == transparentColor && imgPrepared) {
+                    continue;
+                }
                 auto* pixel = subLine + x;
-                const auto* color = ColorMap->Colors + gifLine[x];
+                const auto* color = ColorMap->Colors + colorIndex;
                 pixel->r = color->Red;
                 pixel->g = color->Green;
                 pixel->b = color->Blue;
                 pixel->a = gifLine[x] == transparentColor ? 0 : 255;
             }
-            for (int x = endX; x < width; ++x) {
-                line[x] = bgRGBA;
+            if (!imgPrepared) {
+                for (int x = endX; x < width; ++x) {
+                    line[x] = bgRGBA;
+                }
             }
         }
-        for (int y = endY; y < height; ++y) {
-            auto line = image.get() + y * width;
-            for (int x = 0; x < width; x++) {
-                line[x] = bgRGBA;
+        if (!imgPrepared) {
+            for (int y = endY; y < height; ++y) {
+                auto line = image.get() + y * width;
+                for (int x = 0; x < width; x++) {
+                    line[x] = bgRGBA;
+                }
             }
         }
         
-        std::string newName = name;
-        char buf[32];
-        sprintf(buf, "%d", i);
-        newName.append(buf);
-        newName.append(".png");
+        saveSubImage(name, i, width, height, image.get());
         
-        writePng(newName.c_str(), width, height, image.get());
+        if (!imgPrepared) {
+            images.push_back(image);
+        }
+        
+        if (i >= GifFile->ImageCount) {
+            break;
+        }
+
+        const int disposalMode = hasGCB ? gcb.DisposalMode : DISPOSAL_UNSPECIFIED;
+
+        std::shared_ptr<RGBA[]> fromImage = image;
+        bool needCopy = false;
+        if (disposalMode == DISPOSE_DO_NOT) {
+            doNotDispose = (int)images.size() - 1;
+            needCopy = true;
+        }
+        
+        if (disposalMode == DISPOSE_BACKGROUND) {
+            needCopy = true;
+        }
+        
+        if (disposalMode == DISPOSE_PREVIOUS) {
+            needCopy = true;
+            if (doNotDispose >= 0) {
+                fromImage = images[doNotDispose];
+            }
+        }
+        
+        if (needCopy) {
+            // copy to next image
+            std::shared_ptr<RGBA[]> nextImage = std::shared_ptr<RGBA[]>(new RGBA[GifFile->SWidth * GifFile->SHeight]);
+            images.push_back(nextImage);
+            memcpy(nextImage.get(), fromImage.get(), width * height * sizeof(RGBA));
+            if (disposalMode == DISPOSE_BACKGROUND) {
+                for (int y = 0, subheight = endY - startY; y < subheight; ++y) {
+                    auto line = nextImage.get() + (y + startY) * width;
+                    auto subLine = line + startX;
+                    for (int x = 0, subWidth = endX - startX; x < subWidth; ++x) {
+                        subLine[x] = bgRGBA;
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -187,10 +261,18 @@ int main(int argc, const char * argv[]) {
     
     char cwd[1024];
     printf("cwd: %s\n", getcwd(cwd, 1024));
-    writePng("test.png", 4, 4, data);
+    //writePng("test.png", 4, 4, data);
     
-    readGIF("1.gif");
-    readGIF("2.gif");
+    //readGIF("1.gif");
+    //readGIF("2.gif");
+    //readGIF("3.gif");
+    //readGIF("4.gif");
+    
+    readGIF("img/anim_bgnd.gif");
+    readGIF("img/anim_none.gif");
+    readGIF("img/canvas_bgnd.gif");
+    readGIF("img/canvas_none.gif");
+    readGIF("img/canvas_prev.gif");
 
     return 0;
 }
